799ebf9bb2f4043663e9593010ddad1bc40c38a9
Fr13ndSDP <mylab_octave@163.com>
2020-03-18 12:39:27 +0800

    Hello World!

--- OFtutorial00_helloWorld/OFtutorial0.C
+++ OFtutorial00_helloWorld/OFtutorial0.C
@@ -22,27 +22,28 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
-
+//最基本的openfoam头文件之一 包含以使用fvMesh,label等类
+//fv: finite volume的缩写 使用有限体积法是openfoam的特性
 #include "fvCFD.H"
 
 int main(int argc, char *argv[])
 {
-	// Checks the basic folder structure, verifies there is a control dict present, etc.;
-	// also deals with parsing command line arguments and options.
-	// It works by taking an external piece of code, written in $FOAM_SRC/OpenFOAM/include.
-	// The contents of the include file actually look like this:
+    // 检查基本的文档结构确定是否存在controlDict等
+    // 也用来解析命令行参数和选项
+	// 通过执行在 $FOAM_SRC/OpenFOAM/include 中的外部代码来实现
+	// 具体实现代码:
     /*Foam::argList args(argc, argv); - deciphers the arguments passed to the program
-    if (!args.checkRootCase()) - verifies the folder structure
+    if (!args.checkRootCase()) - 确认文档结构
     {
         Foam::FatalError.exit();
     }*/
     #include "setRootCase.H"
 
-	// OpenFOAM screen output is very similar to rudimentary C++ with its std::cout, std::nl and std::endl
-	// being replaced with Foam::Info, Foam::nl, and Foam::endl.
+	// OpenFOAM的输出与C++的std::cout, std::nl and std::endl类似
+	// 被Foam::Info, Foam::nl, and Foam::endl代替.
 	Info << "Hello there, I'm an OpenFOAM program!" << nl
-		 << "You don't need a mesh or anything to run it, just a bare OpenFOAM case will do." << nl
-		 << tab << "This is me again, just creating a tabulated new line, move along." << nl << endl;
+		 << "你不需要生成网格." << nl
+		 << tab << "又是我,只不过在新的一行里,我们继续吧!." << nl << endl;
 
     Info<< "End\n" << endl;
 
